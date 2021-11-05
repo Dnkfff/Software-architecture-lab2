6e0bce6b5735c8b1d1733385fc4f92801e8c2059
17etro <petrpetryk2016@gmail.com>
2021-11-05 20:18:03 +0300

    testing golang:)

--- implementation.go
+++ implementation.go
@@ -1,9 +1,43 @@
-package lab2
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func stringIsValid(inputString string) bool {
+	const regex = `[0-9/\*\^\+\-\(\)]`
+	arrayFromString := strings.Split(inputString, "")
+
+	var stringIsValid bool = true
+
+	for _, val := range arrayFromString {
+		matched, _ := regexp.MatchString(regex, string(val))
+		if !matched {
+			stringIsValid = false
+		}
+	}
+
+	return stringIsValid
+}
 
 // TODO: document this function.
 // PrefixToPostfix converts
 func PrefixToPostfix(input string) (string, error) {
+
+	const invalidError = "The string is invalid"
+
+	stringIsValid := stringIsValid(input)
+
+	if !stringIsValid {
+		fmt.Println(invalidError)
+		return "_", fmt.Errorf(invalidError)
+	}
+
 	return "TODO", fmt.Errorf("TODO")
+}
+
+func main() {
+	PrefixToPostfix("a12312&^(**^%&")
 }
--- implementation_test.go
+++ implementation_test.go
@@ -1,23 +1,23 @@
-package lab2
+// package lab2
 
-import (
-	"fmt"
-	"testing"
+// import (
+// 	"fmt"
+// 	"testing"
 
-	"github.com/stretchr/testify/assert"
-)
+// 	"github.com/stretchr/testify/assert"
+// )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
-	}
-}
+// func TestPrefixToPostfix(t *testing.T) {
+// 	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
+// 	if assert.Nil(t, err) {
+// 		assert.Equal(t, "4 2 - 3 * 5 +", res)
+// 	}
+// }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+// func ExamplePrefixToPostfix() {
+// 	res, _ := PrefixToPostfix("+ 2 2")
+// 	fmt.Println(res)
 
-	// Output:
-	// 2 2 +
-}
+// 	// Output:
+// 	// 2 2 +
+// }
